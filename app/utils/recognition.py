dced1d04cb6437c2478b5f3e5ebdbcb846fa470b
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-14 13:03:56 +0100

    fix: completed recognition util

--- app/utils/recognition.py
+++ app/utils/recognition.py
@@ -6,7 +6,7 @@
 # @Description : something cool
 
 
-from app.utils.db import list_models_and_filter_by_equality
+from app.utils.crud_util import list_model
 from app.dependencies.dependencies import get_recognition_auth_token
 from fastapi import UploadFile
 from app.utils.misc import requester
@@ -19,8 +19,11 @@ import requests
 
 def remote_search_faces(face_image: UploadFile, token: str) -> requests.Response:
     face_image.file.seek(0)
-    payload = {"collection_id": getenv("RECOGNITION_COLLECTION", default=""), \
-        "threshold": int(getenv("FACE_MATCH_THRESHOLD", default=99)), "max_faces": int(getenv("MAX_FACE_MATCHES", 1))}
+    payload = {
+        "collection_id": getenv("RECOGNITION_COLLECTION", default=""),
+        "threshold": int(getenv("FACE_MATCH_THRESHOLD", default=99)), 
+        "max_faces": int(getenv("MAX_FACE_MATCHES", 1))
+    }
     
     files = {"image_file": face_image.file.read()}
     
@@ -36,12 +39,12 @@ def remote_search_faces(face_image: UploadFile, token: str) -> requests.Response
 
 
 def remote_create_person(
-    person: dict,
+    person: dict[str, Any],
     token: str
-):
+) -> requests.Response:
+
     payload = person
     headers = {"Authorization": f"Bearer {token}"}
-    print(payload)
 
     return requester(
         getenv("PERSON_CREATE_URL", ""),
@@ -51,9 +54,17 @@ def remote_create_person(
     )
 
 
-def remote_index_face(face_image_url: str, face_id: str, token: str) -> requests.Response:
-    payload = {"CollectionId": getenv("RECOGNITION_COLLECTION", default=""), \
-        "ExternalImageId": str(face_id), "ImagesURL": [face_image_url]}
+def remote_index_face(
+    face_image_url: str, 
+    face_id: str, 
+    token: str
+) -> requests.Response:
+
+    payload = {
+        "CollectionId": getenv("RECOGNITION_COLLECTION", default=""),
+        "ExternalImageId": str(face_id),
+        "ImagesURL": [face_image_url]
+    }
     
     url = getenv("FACE_INDEX_URL", "")
     headers = {"Authorization": f"Bearer {token}"}
@@ -80,24 +91,23 @@ def remote_detect_faces(face_image: UploadFile, token: str) -> requests.Response
     )
 
 
-def index_account_model(model_list: List[Any]):
+def index_account_model(model_list: List[Any]) -> None:
     for model in model_list:
         face_image_url: str = model.nin_info.image
         user_id: str = model.user_id
         index_face_to_collection(face_image_url, user_id)
 
 
-def re_index_model(db: Session, model: Any, model_name: str, count_col: str):
-    db_model_record = list_models_and_filter_by_equality(
+def re_index_model(db: Session, model: Any, model_name: str, count_col: str) -> None:
+    db_model_record = list_model(
         db, 
-        model,
-        {},
-        model_name,
+        model_to_list=model,
+        list_conditions={},
         count_by_column=count_col
     )
 
     count = int(db_model_record["count"])
-    model_list: List[Any] = db_model_record["result"]
+    model_list: List[Any] = db_model_record["model_list"]
 
     if count % 100:
         rounds = (count // 100) + 1
@@ -107,26 +117,26 @@ def re_index_model(db: Session, model: Any, model_name: str, count_col: str):
     for i in range(1, rounds + 1):
         index_account_model(model_list)
 
-        db_model_record = list_models_and_filter_by_equality(
+        db_model_record = list_model(
             db, 
-            model,
-            {},
-            model_name,
+            model_to_list=model,
+            list_conditions={},
             skip= i * 100,
             count_by_column=count_col
         )
 
-        model_list = db_model_record["result"]
+        model_list = db_model_record["model_list"]
 
 
-def index_face_to_collection(face_image_url: str, face_id: str):
+def index_face_to_collection(face_image_url: str, face_id: str) -> None:
     try:
         token: str = get_recognition_auth_token()
         remote_index_face(face_image_url, face_id, token)
-    except:
+    except Exception:
         pass
 
 
-def detect_single_face(image: UploadFile, token: str):
-    return remote_detect_faces(image, token).json()
+def detect_single_face(image: UploadFile, token: str) -> dict[str, Any]:
+    resp: dict[str, Any] = remote_detect_faces(image, token).json()
+    return resp
 
