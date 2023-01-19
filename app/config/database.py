50f275744b69c729888be120662991d6daacd58c
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-19 23:10:55 +0100

    refactor: database.py

--- app/config/database.py
+++ app/config/database.py
@@ -9,28 +9,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from os import getenv
+from app.config.config import settings
 
-load_dotenv()
 
-CURRENT_ENV = getenv('LOCAL_DEV_ENV', default="False") 
-
-DATABASE_USERNAME = getenv("DATABASE_USERNAME")
-DATABASE_PASSWORD = getenv("DATABASE_PASSWORD")
-
-if CURRENT_ENV == "True":
-    DATABASE_SERVER_PRIVATE_ADDRESS = getenv("LOCAL_DATABASE_SERVER_PRIVATE_ADDRESS")
-else:
-    DATABASE_SERVER_PRIVATE_ADDRESS = getenv("REMOTE_DATABASE_SERVER_PRIVATE_ADDRESS")
-
-DATABASE_PORT = getenv("DATABASE_PORT")
-DATABASE_NAME = getenv("DATABASE_NAME")
-
-DATABASE_URL = f"postgres://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER_PRIVATE_ADDRESS}:{DATABASE_PORT}/{DATABASE_NAME}" # noqa E501
-
-# todo: the sizes should be set in config settings
-engine = create_engine(DATABASE_URL, pool_size=50, max_overflow=85)
+engine = create_engine(
+    settings.database_url, 
+    pool_size=settings.database_pool_size, 
+    max_overflow=settings.database_max_overflow,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
