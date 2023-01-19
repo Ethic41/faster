e2946e26b4d9ef5c5902431c4707d03bc5189131
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-19 23:23:15 +0100

    refactor: dependency.py

--- app/dependencies/dependencies.py
+++ app/dependencies/dependencies.py
@@ -6,27 +6,18 @@
 # @Description : something cool
 
 
-from datetime import timedelta
 from typing import Any, Generator
 from jose import jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from dotenv import load_dotenv
-from os import getenv
 
 from app.config import database as db
+from app.config.config import settings
 from app.user import cruds as users_cruds, schemas as users_schema
 from app.utils.crud_util import CrudUtil
-from app.utils.timing import get_current_datetime
 
 
-load_dotenv()
-
-
-SECRET_KEY = getenv('JWT_SECRET_KEY')
-TOKEN_URL: str = getenv('DOCS_AUTH_URL', default="")
-ALGORITHM = getenv("JWT_ALGORITHM")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.token_url)
 
 
 def get_db() -> Generator[Any, Any, Any]:
@@ -42,98 +33,21 @@ def get_current_user(
     cu: CrudUtil = Depends(CrudUtil)
 ) -> users_schema.UserSchema:
 
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    payload = jwt.decode(
+        token, 
+        settings.jwt_secret_key, 
+        algorithms=[settings.jwt_algorithm]
+    )
+
     user = users_schema.UserSchema.from_orm(
         users_cruds.get_user_by_email(cu, payload['data']['email'])
     )
     return user
 
 
-def verify_vision_token(
-    token: str = Depends(oauth2_scheme)
-) -> dict[str, Any]:
-
-    vision_secret_key = getenv("VISION_JWT_KEY", "")
-    vision_algo = getenv("VISION_JWT_ALG", "")
-    
-    payload: dict[str, Any] = jwt.decode(
-        token, vision_secret_key, 
-        algorithms=[vision_algo]
-    )
-
-    return payload
-
-
-def has_vision_search_perm(
-    payload: dict[str, Any] = Depends(verify_vision_token)
-) -> bool:
-
-    if "can_search_faces" in payload["data"]["permissions"]:
-        return True
-    
-    raise HTTPException(
-        status_code=403, 
-        detail="Unauthorized access"
-    )
-
-
-def has_detect_face_perm(
-    payload: dict[str, Any] = Depends(verify_vision_token)
-) -> bool:
-
-    if "can_detect_image_face" in payload["data"]["permissions"]:
-        return True
-    
-    raise HTTPException(403, detail="Unauthorized access")
-
-
 def get_auth_token(token: str = Depends(oauth2_scheme)) -> str:
     return token
 
-
-def get_nin_auth_token() -> str:
-    expires = get_current_datetime() + \
-        timedelta(hours=int(getenv("TOKEN_LIFE_SPAN", "24")))
-    
-    data = {
-        "exp": expires, 
-        "data": {
-            "permissions": [
-                "can_retrieve_nin_data"
-            ]
-        }
-    }
-    token: str = jwt.encode(
-        data, getenv("NIN_JWT_KEY"), 
-        algorithm=getenv("NIN_JWT_ALG")
-    )
-    return token
-
-
-def get_recognition_auth_token() -> str:
-    expires = get_current_datetime() + \
-        timedelta(hours=int(getenv("TOKEN_LIFE_SPAN", "24")))
-    
-    data = {
-        "exp": expires, 
-        "data": {
-            "permissions": [
-                "can_search_faces", 
-                "can_index_faces", 
-                "can_detect_image_face"
-            ],
-
-            "email": getenv("MACHINE_NAME", "")
-        }
-    }
-
-    token: str = jwt.encode(
-        data, 
-        getenv("VISION_JWT_KEY"), 
-        algorithm=getenv("VISION_JWT_ALG")
-    )
-
-    return token
 
 class HasPermission:
     def __init__(self, perms: list[str]):
