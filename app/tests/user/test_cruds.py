221bec4006af4a4bb5b17adeb767939a37d95d0b
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-27 03:14:14 +0100

    update: test duplicate user creation

--- app/tests/user/test_cruds.py
+++ app/tests/user/test_cruds.py
@@ -7,10 +7,12 @@
 # @Version : 1.0.0
 
 
+from fastapi import HTTPException
 from typing import Any
 from app.user import cruds, schemas
 from app.utils.crud_util import CrudUtil
 from app.tests.utils.utils import gen_user
+import pytest
 
 def test_create_user(crud_util: CrudUtil) -> Any:
     user_data: schemas.UserIn = gen_user()
@@ -37,4 +39,19 @@ def test_create_admin_user(crud_util: CrudUtil) -> Any:
     assert user.is_active is True
     assert hasattr(user, "id")
     assert hasattr(user, "password_hash")
+
+
+def test_create_duplicate_user(crud_util: CrudUtil) -> Any:
+    with pytest.raises(HTTPException):
+        user_data: schemas.UserIn = gen_user()
+        user = cruds.create_user(
+            crud_util,
+            user_data,
+        )
+        assert user.email == user_data.email
+
+        user = cruds.create_user(
+            crud_util,
+            user_data,
+        )
 
